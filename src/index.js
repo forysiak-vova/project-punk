dc11c8b2aadf6f406ae0b03684362c1d6b692e33
forysiak-vova <volodja.forus@gmail.com>
2021-12-27 23:56:47 +0100

    react

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// =============================================================================================
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// const element = React.createElement('div', {
+//    a: 5,
+//    b: 7
+// }, 'Привіт світ');
+// console.log(element);
+// ReactDom.render(element, document.querySelector('#root'));
+
+const elem1 = <span>Привіт</span>
+const elem2 = <span>мір</span>
+
+const jsxElementt =( <div>
+   {elem1 }
+   { elem2}
+</div>)
+console.log(jsxElementt);
